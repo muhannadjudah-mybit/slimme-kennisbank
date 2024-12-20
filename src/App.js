40de9c58debe7022f7492079c802e41d2a45bdd2
muhannadjudah <muhannad.judah@mybit.nl>
2024-12-20 11:13:48 +0100

    WIP

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ const App = () => {
   const [generatedText, setGeneratedText] = useState("");
 
   const handleGenerate = async () => {
-    const url_1 = "http://localhost:5000/generate"; // voor hugging face
     const url_2 = "http://localhost:5000/ask";
 
     try {
@@ -15,7 +14,21 @@ const App = () => {
         question: inputText
       });
       console.log("Response from ask:", response);
-      setGeneratedText(response.data.answer); // generate > generated_text
+      setGeneratedText(response.data.answer);
+    } catch (error) {
+      console.error("Er ging iets mis:", error);
+    }
+  };
+
+  const handleGenerateHuggingface = async () => {
+    const url = "http://localhost:5000/generate";
+
+    try {
+      const response = await axios.post(url, {
+        text: inputText
+      });
+      console.log("Response from ask:", response);
+      setGeneratedText(response.data[0].generated_text);
     } catch (error) {
       console.error("Er ging iets mis:", error);
     }
@@ -23,14 +36,16 @@ const App = () => {
 
   return (
     <div>
-      {/* <h1>Text Generation via Hugging Face GPT-2</h1> */}
-      <h1>Text Generation via AWS bedrock</h1>
+      <h1>Text Generation via AWS bedrock and huggingface</h1>
       <textarea
         value={inputText}
         onChange={e => setInputText(e.target.value)}
         placeholder="Typ je vraag hier..."
       />
       <button onClick={handleGenerate}>Genereer Tekst</button>
+      <button onClick={handleGenerateHuggingface}>
+        Genereer Tekst with python and huggingface
+      </button>
       <p>{generatedText}</p>
     </div>
   );
